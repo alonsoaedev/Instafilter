3fc96cec8fad9579b56c794396ba487acaf76b3a
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-12 10:01:10 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alonso Acosta on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
