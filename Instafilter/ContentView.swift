1230c29b48e4857635dd7d2ddfabcad74ff605dd
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-12 22:44:11 -0600

    How property wrappers become structs

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
         .padding()
     }
