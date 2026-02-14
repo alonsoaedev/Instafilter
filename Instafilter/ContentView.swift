72db7070ef802dc91fffa35d1cf71e2f15aed2bb
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-13 19:45:58 -0600

    Integrate CoreImage with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,26 +5,58 @@
 //  Created by Alonso Acosta on 12/02/26.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Button("Hello, World!") {
-            showingConfirmation.toggle()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) {  }
-        } message: {
-            Text("Select a new color")
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let begingTime = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        
+//        let currentFilter = CIFilter.sepiaTone()
+//        let currentFilter = CIFilter.pixellate()
+        let currentFilter = CIFilter.crystallize()
+        
+        currentFilter.inputImage = begingTime
+        let inputKeys = currentFilter.inputKeys
+        
+        // sepiaTone
+//        currentFilter.intensity = 1
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(1, forKey: kCIInputIntensityKey)
         }
+        
+        // pixellate
+//        currentFilter.scale = 5
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(5, forKey: kCIInputScaleKey)
+        }
+        
+        // crystallize
+        //        currentFilter.radius = 5
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(5, forKey: kCIInputRadiusKey)
+        }
+        
+        guard let outpuImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outpuImage, from: outpuImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
