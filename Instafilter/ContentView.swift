29d47780704909efa72d4870259ef308b85486cf
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-16 14:12:45 -0600

    Load photos from user's photo library

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,19 +5,42 @@
 //  Created by Alonso Acosta on 12/02/26.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pickerItems: [PhotosPickerItem] = []
+    @State private var selectedImages: [Image] = []
+    
     var body: some View {
-        ContentUnavailableView {
-            Label("No snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any snippets yet")
-        } actions: {
-            Button("Create snippet") {
-                //
+        VStack {
+            PhotosPicker(
+//                "Select a picture",
+                selection: $pickerItems,
+                maxSelectionCount: 3,
+//                matching: .images
+                matching: .any(of: [.images, .not(.screenshots)])
+            ) {
+                Label("Select a picture", systemImage: "photo")
             }
-            .buttonStyle(.borderedProminent)
+            
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
         }
     }
 }
