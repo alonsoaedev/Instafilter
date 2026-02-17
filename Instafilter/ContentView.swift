9cc129b660ba799575eee780f813d6ed070b5f8f
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-16 19:19:34 -0600

    Ask the user to leave an App Store review

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,21 +5,15 @@
 //  Created by Alonso Acosta on 12/02/26.
 //
 
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.requestReview) var requestReview
+    
     var body: some View {
-        let example: Image = Image(.example)
-        
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
-            Label("Spread the word about Swift!", systemImage: "swift")
-        }
-        
-        ShareLink(
-            item: example,
-            preview: SharePreview("Boston", image: example),
-        ) {
-            Label("Click to share", systemImage: "airplane")
+        Button("Leave a review") {
+            requestReview()
         }
     }
 }
